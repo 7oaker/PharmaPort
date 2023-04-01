fa17ff112e46c863d5d0f1eb8ffd2c2562a746cc
Klaus <ks.siebeneicher@gmail.com>
2023-04-01 22:12:43 +0200

    finished sample project and set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,178 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [greet, setGreet] = useState('');
+  const [balance, setBalance] = useState();
+  const [depositValue, setDepositValue] = useState('');
+  const [greetingValue, setGreetingValue] = useState('');
+
+  // A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+const provider = ((window.ethereum != null) ? new ethers.providers.Web3Provider(window.ethereum) : ethers.providers.getDefaultProvider());
+const signer = provider.getSigner()
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+
+// The ERC-20 Contract ABI, which is a common contract interface
+// for tokens (this is the Human-Readable ABI format) -> to call functions of smart contract
+//got from artefacts js abi
+const ABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_greeting",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "deposit",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "greet",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_greeting",
+        "type": "string"
+      }
+    ],
+    "name": "setGreeting",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+// The Contract object
+const contract = new ethers.Contract(contractAddress, ABI, signer);
+
+
+
+
+
+
+// MetaMask requires requesting permission to connect users accounts
+useEffect(() =>{
+  const connectWallet = async() =>{
+    await provider.send("eth_requestAccounts", []);
+
+  }
+
+  const getBalance = async() =>{
+// Get the balance of an account (by address or ENS name, if supported by network)
+const balance = await provider.getBalance(contractAddress)
+
+const balanceFormatted = ethers.utils.formatEther(balance)
+setBalance(balanceFormatted)  
+}
+
+const getGreeting = async() =>{
+  // Get the balance of an account (by address or ENS name, if supported by network)
+  const greeting = await contract.greet();
+  setGreet(greeting);
+ 
+  }
+
+  connectWallet().catch(console.error);
+  getBalance().catch(console.error);
+  getGreeting().catch(console.error);
+
+
+
+})
+
+
+
+
+
+
+
+  const handleDepositChange =(e) =>{
+    setDepositValue(e.target.value);
+  }
+
+  const handleGreetingChange =(e) =>{
+    setGreetingValue(e.target.value);
+
+  }
+
+  const handleDepositSubmit = async (e) =>{
+    e.preventDefault();
+    const ethValue =  ethers.utils.parseEther(depositValue);
+    const depositEth = await contract.deposit({value: ethValue});
+    await depositEth.wait();
+    const balance = await provider.getBalance(contractAddress)
+    const balanceFormatted = ethers.utils.formatEther(balance)
+    setBalance(balanceFormatted)  
+    setDepositValue(0);
+
+  }
+
+  const handleGreetingSubmit = async (e) =>{
+    e.preventDefault();
+    const greetingUpdate = await contract.setGreeting(greetingValue);
+    await greetingUpdate.wait()
+    setGreet(greetingValue);
+    setGreetingValue('');
+    console.log(greetingValue);
+  }
+    return (
+      <div className="container">
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col">
+              <h3>{greet}</h3>
+              <p>Contract balance: {balance} ETH</p>
+            </div>
+            <div className="col">
+            <form onSubmit={handleDepositSubmit}>
+              <div className="mb-3">
+                <label htmlFor="number1" className="form-label">Deposit ETH</label>
+                <input type="number" className="form-control" id="number1" placeholder='0' onChange={handleDepositChange} value = {depositValue} />
+                <div id="emailHelp" className="form-text">We'll never share your credentials with anyone else.</div>
+              </div>
+             
+              <button type="submit" className="btn btn-primary">Deposit</button>
+            </form>
+
+            <form className ="mt-5" onSubmit={handleGreetingSubmit}>
+              <div className="mb-3">
+                <label htmlFor="greeting1" className="form-label">Greeting Change</label>
+                <input type="text" className="form-control" id="greeting1" onChange={handleGreetingChange} value = {greetingValue} />
+                <div id="emailHelp" className="form-text">We'll never share your credentials with anyone else.</div>
+              </div>
+             
+              <button type="submit" className="btn btn-primary">Change</button>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
--- contract/README.md
+++ contract/README.md
@@ -1,4 +1,15 @@
 # Sample Hardhat Project
+https://github.com/7oaker/PharmaPort
+
+Hardhat - etherjs (newer, mor innovative)
+https://www.youtube.com/watch?v=LSL69RGeQL0
+https://github.com/rodgtr1/blockchain-tutorial-for-devs
+or
+Truffl - web3js
+https://www.youtube.com/watch?v=3681ZYbDSSk&t=2251s
+https://www.dappuniversity.com/articles/the-ultimate-ethereum-dapp-tutorial
+
+
 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
 how i set up project: 
@@ -21,9 +32,13 @@ npx hardhat run scripts/deploy.js --network localhost
 
 frontend:
 cd ..
+npm run start
 
 
+bootstrap (https://getbootstrap.com/docs/5.1/forms/overview/)
+npm install bootstrap
 
+ethers (https://docs.ethers.org/v5/)
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
